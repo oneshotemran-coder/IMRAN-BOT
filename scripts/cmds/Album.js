b0c4ab8b51a29001737cef2a7736203dc152545e
oneshotemran-coder <oneshotemran@gmail.com>
2026-05-24 04:12:17 +0600

    Update Album.js

--- scripts/cmds/Album.js
+++ scripts/cmds/Album.js
@@ -277,7 +277,7 @@ module.exports = {
      api.sendMessage(
      { body: caption, attachment: fs.createReadStream(filePath) }, event.threadID, () => fs.unlinkSync(filePath), event.messageID);} catch (error) {
      api.sendMessage("❌ | 𝐅𝐚𝐢𝐥𝐞𝐝 𝐭𝐨 𝐝𝐨𝐰𝐧𝐥𝐨𝐚𝐝 𝐭𝐡𝐞 𝐯𝐢𝐝𝐞𝐨, 🥹error, contact MahMUD", event.threadID, event.messageID); }} catch (error) {
-     api.sendMessage("🥹error, contact 𝐅𝐀𝐇𝐈𝐌.", event.threadID, event.messageID);
+     api.sendMessage("🥹error, contact 𝗜𝗠𝗥𝗔𝗡.", event.threadID, event.messageID);
     }
   }
 };
