28cd0114adb198304048527435d10b17aa6264ed
oneshotemran-coder <oneshotemran@gmail.com>
2026-05-24 02:23:28 +0600

    Update trash.js

--- scripts/cmds/trash.js
+++ scripts/cmds/trash.js
@@ -52,7 +52,7 @@ module.exports = {
     threadID, () => fs.unlinkSync(filePath),  messageID );
   } catch (err) {
     console.error(err);
-    api.sendMessage(`🥹error, contact 𝗙𝗔𝗛𝗜𝗠.`, threadID, messageID);
+    api.sendMessage(`🥹error, contact 𝗜𝗠𝗥𝗔𝗡.`, threadID, messageID);
     }
   },
 };
